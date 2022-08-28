4fba6440e48728be495e3f576f5b46b59e3717ff
sahathat <61311677+sahathat@users.noreply.github.com>
2022-08-28 22:41:03 +0700

    spike: security config and jwt

--- back-end/TestAuthenticate/src/main/java/sit/int222/testauthenticate/Controller/TestPassword.java
+++ back-end/TestAuthenticate/src/main/java/sit/int222/testauthenticate/Controller/TestPassword.java
@@ -1,19 +1,25 @@
 package sit.int222.testauthenticate.Controller;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import sit.int222.testauthenticate.Service.UserService;
+import sit.int222.testauthenticate.dto.Response;
 import sit.int222.testauthenticate.dto.User;
+import sit.int222.testauthenticate.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/api")
 public class TestPassword {
 
-    @GetMapping("/encrypt")
+    @GetMapping("/user/encrypt")
     public String encrypt() {
         Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(16,27,2,4096,10);
 
@@ -24,7 +30,7 @@ public class TestPassword {
         return hash;
     }
 
-    @PostMapping("/match")
+    @PostMapping("/user/match")
     public String match(@RequestBody User user) {
         String hash = encrypt();
         Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
@@ -33,5 +39,10 @@ public class TestPassword {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "password does not matched");
         }
         return "yay!!";
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello";
     }
 }
