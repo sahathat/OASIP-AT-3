cbfaf3809735ed91600d88e798e318025f209316
sahathat <61311677+sahathat@users.noreply.github.com>
2022-09-09 19:48:09 +0700

    backend r2 v4.5

---
+++ back-end-intregate/At-3-Booking/src/main/java/sit/int221/at3/config/SpringSecurityConfiguration.java
@@ -0,0 +1,61 @@
+package sit.int221.at3.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import sit.int221.at3.filter.CustomJwtAuthenticationFilter;
+import sit.int221.at3.services.UserService;
+import sit.int221.at3.utils.JwtAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CustomJwtAuthenticationFilter customJwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    public Argon2PasswordEncoder passwordEncoder(){
+        return new Argon2PasswordEncoder(16,27,2,4096,10);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+//                .antMatchers("/helloadmin").hasRole("ADMIN")
+//                .antMatchers("/hellostudent").hasAnyRole("STUDENT","ADMIN")
+                .antMatchers("/api/users/signin","/api/users/signup","/api/users/refresh").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).
+                and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().addFilterBefore(customJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
+
