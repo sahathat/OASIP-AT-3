71cf37d546381e4dc8361b7839a76bd949a02da2
sahathat <61311677+sahathat@users.noreply.github.com>
2022-09-26 10:08:30 +0700

    backend r2 v5

--- back-end-intregate/At-3-Booking/At-3-Booking/src/main/java/sit/int221/at3/config/ApplicationConfig.java
+++ back-end-intregate/At-3-Booking/At-3-Booking/src/main/java/sit/int221/at3/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import sit.int221.at3.utils.ListMapper;
 
 @SpringBootApplication
@@ -22,14 +21,4 @@ public class ApplicationConfig {
         return ListMapper.getInstance();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
 }
